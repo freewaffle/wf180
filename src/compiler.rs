b23eda0142b496b1c13455a589947017e54e06ec
freewaffle <curtis.cmux@ya.ru>
2026-08-05 17:35:18 +0400

    add `Token` enum

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,12 +1,28 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[repr(u8)]
+enum Token {
+    Identifier(String),
+    String(String),
+    Float(f32),
+    Integer(i32),
+
+    Add,
+    Sub,
+    Mul,
+    Div,
+
+    Dot,
+    Comma
+}
+
 pub fn compile_from_file(file: File) -> Vec<u8> {
-    let mut reader = BufReader::new(file);
+    let reader = BufReader::new(file);
     let lines = reader.lines().map(|line| line.unwrap());
     
     for line in lines {
-        println!("{line}");
+        println!(">>> {line}");
     }
 
     Vec::new()
