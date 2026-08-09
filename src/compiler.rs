bfa416f3328c177e5e444971a06125b4172bb784
freewaffle <curtis.cmux@ya.ru>
2026-08-09 23:26:38 +0400

    implement identifier string collection

--- src/compiler.rs
+++ src/compiler.rs
@@ -27,7 +27,7 @@ pub fn compile_from_file(file: File, filename: &String) -> Vec<u8> {
     let mut current_line: usize = 1;
     
     for input_line in input_lines {
-        println!(">>> {input_line}");
+        println!("\n>>> {input_line}");
 
         let mut chars = input_line.chars().peekable();
         let mut line: Vec<Token> = Vec::new();
@@ -44,8 +44,10 @@ pub fn compile_from_file(file: File, filename: &String) -> Vec<u8> {
 
         // `for ch in chars` takes ownership
         while let Some(ch) = chars.next() {
-            // identifier
-            if ch.is_ascii_alphabetic() {
+            let identifier = ch.is_ascii_alphabetic() || ch == '_';
+            let line_comment = ch == '#';
+
+            if identifier {
                 let mut ident: String = String::with_capacity(MAX_IDENTIFIER_LENGTH);
 
                 ident.push(ch);
@@ -53,17 +55,26 @@ pub fn compile_from_file(file: File, filename: &String) -> Vec<u8> {
                 while let Some(ch) = chars.peek() {
                     if ident.len() >= MAX_IDENTIFIER_LENGTH {
                         error!(format!("too long identifier (max length is {MAX_IDENTIFIER_LENGTH} symbols)"));
-                        break
+                        break;
                     }
 
                     if !ch.is_ascii_alphabetic() {
-                        break
+                        break;
                     }
+
+                    ident.push(*ch);
+                    chars.next();
                 }
+
+                println!("> ident: {ident}");
+            }
+
+            if line_comment {
+                break;
             }
         }
 
-        if !line.is_empty() {
+        if !line.is_empty() && !bad {
             lines.push(line);
         }
 
